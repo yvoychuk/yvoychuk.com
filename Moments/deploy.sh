8897e6e2b3d4514e28ad39558f6087a156f0836b
yaroslav <y.voychuk@gmail.com>
2019-06-22 20:14:10 +0300

    Add first simple memory

--- Moments/deploy.sh
+++ Moments/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 HOST=yaroslav@104.248.43.189
 DIRPATH=~/Moments/
-FILEPATH=./server.js
 if ssh $HOST stat $DIRPATH \> /dev/null 2\>\&1
   then
     echo "exists"
   else
     ssh $HOST mkdir $DIRPATH
 fi
-scp $FILEPATH $HOST:$DIRPATH
+scp package.json server.js $HOST:$DIRPATH
+ssh $HOST "cd $DIRPATH; npm install; pm2 restart server"
--- Moments/server.js
+++ Moments/server.js
@@ -1,18 +1,47 @@
-const Hapi = require("@hapi/hapi");
+const path = require('path');
+const Hapi = require('@hapi/hapi');
+const Hoek = require('@hapi/hoek');
 
 const init = async () => {
   const server = Hapi.server({
     port: 3001,
-    host: "localhost"
+    host: "localhost",
+    routes: {
+      files: {
+        relativeTo: path.join(__dirname, 'static'),
+      },
+    },
   });
+
+  await server.register(require('@hapi/vision'));
+  await server.register(require('@hapi/inert'));
 
   server.route({
     method: "GET",
-    path: "/",
+    path: "/moments",
     handler: (request, h) => {
-      return "Hello World!";
-    }
+      return h.view('index');
+    },
   });
+
+  server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: path.join(__dirname, 'app/static'),
+        listing: true,
+      },
+    },
+  })
+
+  server.views({
+    engines: {
+      html: require('handlebars'),
+    },
+    relativeTo: __dirname,
+    path: 'app',
+  })
 
   await server.start();
   console.log("Server running on %s", server.info.uri);

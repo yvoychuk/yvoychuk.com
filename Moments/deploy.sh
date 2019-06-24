815f5148725b826feb2348d0df980720f24efce5
yaroslav <y.voychuk@gmail.com>
2019-06-24 11:20:24 +0300

    Allow deploy dirs

--- Moments/deploy.sh
+++ Moments/deploy.sh
@@ -7,5 +7,5 @@ if ssh $HOST stat $DIRPATH \> /dev/null 2\>\&1
   else
     ssh $HOST mkdir $DIRPATH
 fi
-scp package.json server.js $HOST:$DIRPATH
+scp -rp package.json server.js ./app $HOST:$DIRPATH
 ssh $HOST "cd $DIRPATH; npm install; pm2 restart server"
